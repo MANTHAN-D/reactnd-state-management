2e7812cd6e850cab1a1611338fb84bab3eb1ab1f
Manthan <manthanh.doshi@outlook.com>
2018-07-02 00:12:10 +0000

    Slightly improvized js syntax

--- src/ListPersonalFavorites.js
+++ src/ListPersonalFavorites.js
@@ -7,7 +7,7 @@ class ListPersonalFavorite extends Component {
    	<ol>
      {this.props.profiles.map(profile => (
       	<li key={profile.id}>
-      		{this.props.users[profile.userID].name}'s favorite movie is {this.props.movies[profile.favoriteMovieID].name}.
+      		{`${this.props.users[profile.userID].name}'s favorite movie is ${this.props.movies[profile.favoriteMovieID].name}`}.
       	</li>
       ))}
     </ol>
